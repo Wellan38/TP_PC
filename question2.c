dac457c715be81297f50cc6d9f4ed5663e2bcacd
Bento Alexandre <abento@if501-219-08.insa-lyon.fr>
2016-04-06 13:31:06 +0200

    Corrected parsePrimeFile and added numbers.txt

--- question2.c
+++ question2.c
@@ -10,7 +10,9 @@ int main(void)
     // your code goes  here: open the text file (e.g.  with fopen() ),
     // then read each line (e.g. with fgets() ), turn it into a number
     // (e.g. with atoll() ) and then pass it to print_prime_factors.
-	premier(150);
+	//premier(150);
+	
+	parsePrimeFile("numbers.txt");
 
     return 0;
 }
@@ -20,7 +22,7 @@ void parsePrimeFile(char* filename)
 	FILE* file = fopen(filename, "r");
 	uint64_t number;
 	
-	if(file)
+	while(file)
 	{
 		
 		fscanf(file, "%ld", &number);
